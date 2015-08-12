8bb6343960e0d695948935bbd9393e9c0e4896e4
Propaganistas <propaganistas@gmail.com>
2015-08-12 16:22:42 +0200

    Cleanup leftovers InvalidParameterException

--- src/Exceptions/InvalidParameterException.php
+++ src/Exceptions/InvalidParameterException.php
@@ -1,8 +1,3 @@
 <?php namespace Propaganistas\LaravelPhone\Exceptions;
 
-class InvalidParameterException extends \Exception {
-
-	public function __construct($message) {
-		parent::__construct($message);
-	}
-}
+class InvalidParameterException extends \Exception {}
