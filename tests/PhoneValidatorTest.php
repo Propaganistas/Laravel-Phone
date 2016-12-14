e6ef384fb0f94f478d1a2cdd235a6de00d8c462b
Ashleigh Newman-Jones <ashnewman-jones@hotmail.co.uk>
2016-12-14 17:08:21 +0000

    Upgrade to libphonenumber-for-php v8.0 and fix phpunit warnings (#42)
    * Upgrade to libphonenumber-for-php v8.0 and fix phpunit warnings
    
    * Use phpunit annotations to fix travis failure
    
    * Include php 7.1 in travis tests
    
    * Changes as requested in review
    
    * Use expectException() and remove phpunit ^4.0 from composer.json

--- tests/PhoneValidatorTest.php
+++ tests/PhoneValidatorTest.php
@@ -254,7 +254,7 @@ class PhoneValidatorTest extends TestCase
 
     public function testValidatePhoneNoDefaultCountryNoCountryField()
     {
-        $this->setExpectedException('Propaganistas\LaravelPhone\Exceptions\NoValidCountryFoundException');
+        $this->expectException('Propaganistas\LaravelPhone\Exceptions\NoValidCountryFoundException');
 
         // Validator with no country field or given country.
         $this->performValidation([
@@ -347,7 +347,7 @@ class PhoneValidatorTest extends TestCase
 
     public function testValidatePhoneFaultyParameters()
     {
-        $this->setExpectedException('Propaganistas\LaravelPhone\Exceptions\InvalidParameterException');
+        $this->expectException('Propaganistas\LaravelPhone\Exceptions\InvalidParameterException');
 
         // Validator with given country, correct type, faulty parameter.
         $this->performValidation([
