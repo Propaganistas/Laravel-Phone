75fb4687e3744ae496a57a26edcff8bdd13a83dd
Joe <104938042+lrljoe@users.noreply.github.com>
2023-04-26 04:34:50 +0100

    Update PhoneNumber.php

--- src/PhoneNumber.php
+++ src/PhoneNumber.php
@@ -34,15 +34,6 @@ class PhoneNumber implements Jsonable, JsonSerializable
         $this->number = $number;
         $this->countries = Arr::wrap($country);
     }
-
-    public function getCarrier(): string|null
-    {
-        
-        $phoneUtil = PhoneNumberUtil::getInstance()->parse($this->number, $this->getCountry());
-        $carrierMapper = PhoneNumberToCarrierMapper::getInstance();
-        return $carrierMapper->getNameForNumber($phoneUtil, "en");
-    }
-
     
     public function getCountry(): string|null
     {
