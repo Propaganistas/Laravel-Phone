57ec1db0e9297d8ba211dd8f28a38edd6be5d36e
Simon J <2857218+mwnciau@users.noreply.github.com>
2024-01-15 09:19:53 +0000

    Reorder setParameters checks to fix mobile and fixed_line rules being interpreted as countries (#242)
    * Reorder setParameters checks
    
    * Add test for broken mobile and fixed_line rules
    
    ---------
    
    Co-authored-by: Simon Jarrett <simonjarrett@capuk.org>

--- tests/RuleTest.php
+++ tests/RuleTest.php
@@ -184,6 +184,22 @@ class RuleTest extends TestCase
     }
 
     /** @test */
+    public function it_allows_data_field_names_to_be_parameters()
+    {
+        $base = (new Phone)->setValidator(validator([
+            'mobile' => null,
+            'fixed_line' => null,
+            'lenient' => null,
+            'international' => null,
+        ]));
+
+        $rule = $base->setParameters(['mobile', 'fixed_line', 'lenient', 'international']);
+        $this->assertEquals(['mobile', 'fixed_line'], $this->getProtectedProperty($rule, 'allowedTypes'));
+        $this->assertTrue($this->getProtectedProperty($rule, 'lenient'));
+        $this->assertTrue($this->getProtectedProperty($rule, 'international'));
+    }
+
+    /** @test */
     public function it_treats_string_validation_parameters_case_insensitive()
     {
         $base = (new Phone)->setValidator(validator(['foo' => null]));
