2448eab9e4b691cb014b2a8fded5d778c9278f43
Propaganistas <Propaganistas@users.noreply.github.com>
2025-02-28 09:34:10 +0100

    improve tests

--- tests/E164PhoneNumberCastTest.php
+++ tests/E164PhoneNumberCastTest.php
@@ -11,56 +11,83 @@ use UnexpectedValueException;
 class E164PhoneNumberCastTest extends TestCase
 {
     #[Test]
-    public function it_mutates_to_e164_number()
+    public function it_supports_no_parameters()
     {
         $model = new ModelWithE164Cast;
         $model->phone = '+32 12 34 56 78';
         $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
 
         $model = new ModelWithE164Cast;
         $model->phone = new PhoneNumber('+32 12/34.56.78');
         $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
     }
 
     #[Test]
-    public function it_mutates_to_e164_number_with_implicit_country_field()
+    public function it_supports_implicit_country_field()
     {
         $model = new ModelWithE164Cast;
         $model->phone_country = 'BE';
         $model->phone = '012 34 56 78';
         $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
 
         $model = new ModelWithE164Cast;
         $model->phone_country = 'BE';
         $model->phone = new PhoneNumber('+32 12/34.56.78');
         $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
     }
 
     #[Test]
-    public function it_mutates_to_e164_number_with_explicit_country_field()
+    public function it_supports_explicit_country_field()
     {
         $model = new ModelWithE164CastAndCountryField;
         $model->country = 'BE';
         $model->phone = '012 34 56 78';
         $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
 
         $model = new ModelWithE164CastAndCountryField;
         $model->country = 'BE';
         $model->phone = new PhoneNumber('+32 12/34.56.78');
         $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
     }
 
     #[Test]
-    public function it_gets_phone_object()
+    public function it_supports_explicit_country()
     {
-        $model = new ModelWithE164Cast;
-        $model->setRawAttributes(['phone' => '+3212345678']);
-        $this->assertIsObject($model->phone);
-        $this->assertEquals(PhoneNumber::class, get_class($model->phone));
+        $model = new ModelWithE164CastAndCountry;
+        $model->phone = '012 34 56 78';
+        $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
+
+        $model = new ModelWithE164CastAndCountry;
+        $model->phone = new PhoneNumber('+32 12/34.56.78');
+        $this->assertEquals('+3212345678', $model->getAttributes()['phone']);
+        $this->assertInstanceOf(PhoneNumber::class, $model->phone);
+        $this->assertEquals('012 34 56 78', $model->phone->formatNational());
+        $this->assertEquals('BE', $model->phone->getCountry());
     }
 
     #[Test]
-    public function it_throws_when_accessing_non_international_value()
+    public function it_throws_when_accessing_countryless_value()
     {
         $model = new ModelWithE164Cast();
         $model->setRawAttributes(['phone' => '012 34 56 78']);
@@ -92,5 +119,12 @@ class ModelWithE164CastAndCountryField extends Model
 {
     protected $casts = [
         'phone' => E164PhoneNumberCast::class.':country',
+    ];
+}
+
+class ModelWithE164CastAndCountry extends Model
+{
+    protected $casts = [
+        'phone' => E164PhoneNumberCast::class.':BE',
     ];
 }
