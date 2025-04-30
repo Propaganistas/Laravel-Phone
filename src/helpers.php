22c686f5dcc7b5262074c751fa7d16253c039a9f
Lloric Mayuga Garcia <lloricode@gmail.com>
2025-04-30 10:40:11 +0800

    Fix phpstan concern for dynamic return in helper

--- src/helpers.php
+++ src/helpers.php
@@ -6,6 +6,7 @@ use Propaganistas\LaravelPhone\PhoneNumber;
 if (! function_exists('phone')) {
     /**
      * @param  array<string>|string|null  $country
+     * @return ($format is null ? \Propaganistas\LaravelPhone\PhoneNumber : string)
      */
     function phone(string $number, array|string|null $country = null, PhoneNumberFormat|string|null $format = null): PhoneNumber|string
     {
